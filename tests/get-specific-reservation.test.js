ead6089eece97fa8f553cc4647e37965baf92f57
Joel Jossie <joeljossie@gmail.com>
2021-02-11 21:32:49 -0700

    tick

--- tests/get-specific-reservation.test.js
+++ tests/get-specific-reservation.test.js
@@ -27,7 +27,7 @@ let postMakeReservation = {
     simple: false,
 };
 
-it('Trying to get reservation at ${reservationsUrl}', async () =>{
+it(`Trying to get reservation at ${reservationsUrl}`, async () =>{
     
     // Make the Reservation and get its ID
     var reservationId = await rp(postMakeReservation);
